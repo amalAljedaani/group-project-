9d137ae1fc0cf6910a6ba7c83f936cd065170653
Amal <71779079+amalAljedaani@users.noreply.github.com>
2023-05-19 17:15:10 +0300

    Update Edge.java

--- Edge.java
+++ Edge.java
@@ -3,15 +3,18 @@ package GraphFramework;
 
 
 public class Edge {
+        //initialize variables
         Vertex source;
         Vertex destination;
         int weight;
-
+        
+        //constructor to create an edge taking the 2 vertices and their weights 
         public Edge(Vertex source, Vertex destination, int weight) {
             this.source = source;
             this.destination = destination;
             this.weight = weight;
         }
+       
     public String display_info (){
        return "(--> "+destination.getLabel()+" weight:"+weight+" ) ";
     }
