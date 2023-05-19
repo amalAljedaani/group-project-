82016265079b249a96bf828b753d426e1be262e6
Amal <71779079+amalAljedaani@users.noreply.github.com>
2023-05-19 17:16:51 +0300

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -5,25 +5,24 @@ import java.util.LinkedList;
 
 
 public class Vertex {
-    
+     //initialize variables 
      public String Label ;
      boolean isVisited = true;
      LinkedList<Edge> adjLists=new LinkedList<Edge>();
-    
+    //constructor to create a vertex using a givin string as its label
     public Vertex (String Label){
         this.Label = Label;
     }
 
-    public   String getLabel() {
+    public String getLabel() {
         return Label;
     }
     
     public String display_info (){
-        return Label;
-        
+        return Label;    
     }
   
-   
+    
     public boolean isVisited(String Label) {
         return Label == this.Label;
     }
