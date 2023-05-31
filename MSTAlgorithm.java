3f868dbef9e7e74126bfc3226144d8026c12d2db
Deem Faisal <131193174+deemfaisall@users.noreply.github.com>
2023-05-31 22:26:16 +0300

    MSTAlgorithm.java

--- MSTAlgorithm.java
+++ MSTAlgorithm.java
@@ -4,16 +4,15 @@ package GraphFramework;
 
 import java.util.*;
 public abstract class MSTAlgorithm {
-    public Graph graph;
-    public List<Edge> MSTResultList; 
+    public Graph graph; // Declare a variable to store the graph
+    public List<Edge> MSTResultList; // Declare a variable to store the resulting MST
 
+    // Consturctor
     public MSTAlgorithm(Graph graph) {
-        this.graph = graph;
-        this.MSTResultList= new ArrayList<>();
+        this.graph = graph; // Assign the graph to the variable
+        this.MSTResultList= new ArrayList<>(); // Initialize the list to store the MST
     }
    
-   public abstract List<Edge> displayResultingMST();
-   
+   public abstract List<Edge> displayResultingMST(); // Abstract method to display the resulting MST
    
 }
-
